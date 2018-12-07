96c3081c5912eb4fc43fcc35b46c9571bfbef790
Daniel Thompson <dwt02011@mymail.pomona.edu>
2018-12-06 21:42:10 -0500

    Updates

---
+++ precache-manifest.5eb824360c85a72f6aa235ad03cb89ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bb90536c20586cad7eec",
+    "url": "/spotify/static/js/runtime~main.bb90536c.js"
+  },
+  {
+    "revision": "ef7d15d85e5c1d3a7a71",
+    "url": "/spotify/static/js/main.ef7d15d8.chunk.js"
+  },
+  {
+    "revision": "7ca78344f542b60d623f",
+    "url": "/spotify/static/js/1.7ca78344.chunk.js"
+  },
+  {
+    "revision": "ef7d15d85e5c1d3a7a71",
+    "url": "/spotify/static/css/main.bbe439fa.chunk.css"
+  },
+  {
+    "revision": "09785bfe96386eea5f0e302f1f4bd192",
+    "url": "/spotify/index.html"
+  }
+];
